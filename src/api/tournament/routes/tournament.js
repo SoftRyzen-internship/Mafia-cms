4a521316ad5e481d323add21a6af99ef8b909959
veronikanos <vveronikanika@gmail.com>
2023-09-06 17:16:02 +0300

    feat: add tournaments, delete type isVisible

---
+++ src/api/tournament/routes/tournament.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tournament router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tournament.tournament');
