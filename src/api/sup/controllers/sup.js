cd94a3f08251545f3ba29519a06ec70aca4220ff
andrey-lawyer <andrey-1@ukr.net>
2023-09-06 10:10:18 +0300

    fix image cups

--- src/api/sup/controllers/sup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sup controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sup.sup');
--- src/api/sup/routes/sup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sup router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sup.sup');
