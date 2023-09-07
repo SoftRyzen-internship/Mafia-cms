e6d1a0d03d1f80c0df3cdd11b55d14dc2a73addb
andrey-lawyer <andrey-1@ukr.net>
2023-09-07 09:52:30 +0300

    сhange schedule

---
+++ src/api/work-schedule/routes/work-schedule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-schedule router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::work-schedule.work-schedule');
