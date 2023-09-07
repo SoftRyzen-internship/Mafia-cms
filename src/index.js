a6fc58d1d56423610818f351766f060f5b337e33
veronikanos <vveronikanika@gmail.com>
2023-09-07 11:41:58 +0300

    fix: add tournaments content

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 'use strict';
 
-const conditions = [
-  {
-    displayName: 'Max Published Presenters',
-    name: 'max-published-presenters',
-    plugin: 'admin',
-    async handler(user) {
-      // Check the total number of published "Presenter" entries
-      const presenterCount = await strapi.query('presenter').count({
-        published_at_null: false,
-      });
-
-      // Limit the total published entries to no more than 3
-      return presenterCount <= 3;
-    },
-  },
-  // Other conditions...
-];
-
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -34,8 +16,5 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap(/*{ strapi }*/) {
-    await strapi.admin.services.permission.conditionProvider.registerMany(conditions);
-
-  },
+  bootstrap(/*{ strapi }*/) {},
 };
