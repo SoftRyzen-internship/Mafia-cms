4a6753061b384a69d73432b6fcd7ad84c24918d4
andrey-lawyer <andrey-1@ukr.net>
2023-09-07 17:24:28 +0300

    add settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,23 @@
 module.exports = ({ env }) => ({
-
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
       },
+    },
+  },
+
+  graphql: {
+    config: {     
+      defaultLimit: 100
     },
   },
 });
