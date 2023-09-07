9a7fcf8c42f988a23d10baa2bc5d43fd44c20345
veronikanos <vveronikanika@gmail.com>
2023-09-07 11:09:15 +0300

    feat: add permission for admin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const conditions = [
+  {
+    displayName: 'Max Published Presenters',
+    name: 'max-published-presenters',
+    plugin: 'admin',
+    async handler(user) {
+      // Check the total number of published "Presenter" entries
+      const presenterCount = await strapi.query('presenter').count({
+        published_at_null: false,
+      });
+
+      // Limit the total published entries to no more than 3
+      return presenterCount <= 3;
+    },
+  },
+  // Other conditions...
+];
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +34,8 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap(/*{ strapi }*/) {
+    await strapi.admin.services.permission.conditionProvider.registerMany(conditions);
+
+  },
 };
